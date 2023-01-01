f805e42ea052a2cfef78f84382405387e784bf0d
malcolmwilson8 <malcolmwilsonmcg@gmail.com>
2023-01-01 23:26:50 +0000

    Added for loop responding to colour picker

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
 let spaceBar = document.getElementsByTagName("button")[0];
 //** Declaring the variable currentColour with document.getElementsByClassName turns it into an array, which we can loop through afterwards  */
-let currentColour = document.getElementsByClassName("hex-input");
-//** Do the same for the colour variable, which will be looped through for each element  */
+let currentColour = document.getElementsByClassName("current-hex");
+//** Do the same for the colour variable and hexChange, which will be looped through for each element  */
 let colour = document.getElementsByClassName("colour-content");
-const lockit = document.getElementById("toggle-lock");
+let hexChange = document.getElementsByClassName("hex-changer")
+const lockit = document.getElementsByClassName("toggle-lock");
 const copyit = document.getElementById("copy-colour");
 
 // Added functionality which activates only if the spacebar is pressed, and performs the changeColours function
-document.body.onkeyup = function(e){if(e.keyCode == 32){changeColours()}};
+let spacePressed = document.body.onkeyup = function(e){if(e.keyCode == 32){changeColours()}};
 // Added auto load event listener which automatically generates multiple colours
 document.addEventListener("DOMContentLoaded", changeColours());
 
-lockit.addEventListener('click', () => {})
+// Trying to add lock functionality
+// lockit.addEventListener('click', () => {})
 
 function changeColours(){
     for(let x = 0; x < colour.length; x++){
-        //** math.floor will return a whole number. Math.random will provide a random number out of the 16777215 possible hex code combinations which we can assign to each div */
-        let randomHex = Math.floor(Math.random()*16777215).toString(16);
+        //** math.floor will return a whole number. Math.random will provide a random number out of the 16777216 possible hex code combinations which we can assign to each div, start of the number will be padded with 6 0's, so that the resulting string always reaches 6 characters.  */
+        let randomHex = Math.floor(Math.random()*16777216).toString(16).padStart(6, '0');
         // Set the colour of each div (by iterating through each with x) to a random hex colour
         colour[x].style.backgroundColor = '#' + randomHex;
-        // Update hex code info for each div to show the colour it currently is
+        // Update hex code text displayed for each div to show the colour it currently is
         currentColour[x].textContent = '#' + randomHex;
+        hexChange[x].value = currentColour[x].textContent;
     }
+}
+
+// For loop which responds to users manually selecting a colour within the colour picker
+for(let x = 0; x < colour.length; x++){
+    hexChange[x].addEventListener('change', function(){
+        colour[x].style.backgroundColor = this.value;})  
 }
